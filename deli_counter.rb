e1073c9891c0bb00ee8d82c7f3dd206f452333af
Darcy Linde <lindedarcy@gmail.com>
2019-02-22 19:28:43 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,7 @@ def line(array)
     puts "The line is currently empty."
   else 
     str = "The line is currently:"
-    array.each_by_index do |person, index|
+    array.each_with_index do |person, index|
       str + " #{index + 1}. #{person}"
     end
   end
