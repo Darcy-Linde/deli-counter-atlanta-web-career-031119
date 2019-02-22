51e8ef8fba685d5fda90ebbaa2f18ccf3908eaa4
Darcy Linde <lindedarcy@gmail.com>
2019-02-22 19:28:36 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+katz_deli = []
+
+#line
+def line(array)
+  if array.size == 0
+    puts "The line is currently empty."
+  else 
+    str = "The line is currently:"
+    array.each_by_index do |person, index|
+      str + " #{index + 1}. #{person}"
+    end
+  end
+end
